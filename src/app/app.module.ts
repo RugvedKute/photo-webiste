482ee9c03587c4a4e16bd5d2869703244e88e1e1
clarkkentttt <rugved.k@sankeysolutions.com>
2023-09-05 20:02:13 +0530

    This is the first day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
